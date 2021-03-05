3b1f358fad813b7399a1035e137b42be66c40941
Pushpak <pushpakkumar3667@gmail.com>
2021-03-05 06:23:25 +0530

    first commit

---
+++ my-app/src/app/resform/resform.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resform',
+  templateUrl: './resform.component.html',
+  styleUrls: ['./resform.component.css']
+})
+export class ResformComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
